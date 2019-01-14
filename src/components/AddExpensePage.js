394a76104497a7f3dd447fce590a810b6864d12e
Adam Morelli <amorelli@gmail.com>
2019-01-14 14:05:41 -0500

    Setup test database environment

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { addExpense } from '../actions/expenses'; // Import addExpense action generator
+import { startAddExpense } from '../actions/expenses'; // Import startAddExpense action generator
 
 const AddExpensePage = (props) => (
   <div>
     <h1>Add Expense</h1>
     <ExpenseForm 
       onSubmit={(expense) => { // Expense object received from ExpenseForm {description, amount, note, createdAt}
-        props.dispatch(addExpense(expense));
+        props.dispatch(startAddExpense(expense));
         props.history.push('/'); // Programmatically change pages, takes target page argument
       }}
     />
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,50 +3,94 @@ import * as firebase from 'firebase';
 //  INITIALIZE
 //
 const config = {
-  apiKey: "AIzaSyCFm5nRHYNwRjh124QRF2NbkqqBd_T2kak",
-  authDomain: "expensify-e4d0a.firebaseapp.com",
-  databaseURL: "https://expensify-e4d0a.firebaseio.com",
-  projectId: "expensify-e4d0a",
-  storageBucket: "expensify-e4d0a.appspot.com",
-  messagingSenderId: "885962097218"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
 
 const database = firebase.database();
 
+export { firebase, database as default };
+
+//
+//  DUMMY DATA
+//
+// database.ref('expenses').push({
+//   description: 'Rent',
+//   amount: 1200,
+//   createdAt: 12345,
+//   note: '',
+// });
+
+// database.ref('expenses').push({
+//   description: 'Food',
+//   amount: 500,
+//   createdAt: 12345,
+//   note: '',
+// });
+
+// database.ref('expenses').push({
+//   description: 'Electricity',
+//   amount: 800,
+//   createdAt: 12345,
+//   note: '',
+// });
+
+// database.ref("notes").push({
+//   title: 'Course topics',
+//   body: 'React, Javascript, Python'
+// });
+
+// const firebaseNotes = {
+//   notes: {
+//     ab123: {
+//       title: 'My first note',
+//       body: 'This is my note'
+//     },
+//     5678: {
+//       title: 'Another note',
+//       body: 'This is another note'
+//     }
+//   }
+// };
+
 //
 //  SET INITIAL DATA
 //
 // ref() gives us a reference to a specific part of our database (Different collections)
 // Not passing anything in is a reference to the root
-database.ref().set({
-  name: 'Adam',
-  age: 33,
-  isCool: true,
-  stressLevel: 7,
-  location: {
-    city: 'Raleigh',
-    country: 'United States'
-  },
-  job: {
-    title: 'Developer',
-    company: 'Google'
-  }
-}).then(() => {
-  console.log('Data is saved.');
-}).catch((e) => {
-  console.log('This failed -> ', e)
-});
+// database.ref().set({
+//   name: 'Adam',
+//   age: 33,
+//   isCool: true,
+//   stressLevel: 7,
+//   location: {
+//     city: 'Raleigh',
+//     country: 'United States'
+//   },
+//   job: {
+//     title: 'Developer',
+//     company: 'Google'
+//   }
+// }).then(() => {
+//   console.log('Data is saved.');
+// }).catch((e) => {
+//   console.log('This failed -> ', e)
+// });
 
 //
 //  UPDATE
 //
-database.ref().update({
-  stressLevel: 9,
-  'job/company': 'Amazon',
-  'location/city': 'Seattle'
-});
+// database.ref().update({
+//   stressLevel: 9,
+//   'job/company': 'Amazon',
+//   'location/city': 'Seattle'
+// });
 
 //
 // UPDATE EXAMPLE - (Update will update, add and remove properties)
